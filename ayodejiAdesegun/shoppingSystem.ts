de957e31c5c264758a33ecc96567a47a85a266db
Ayodeji Adesegun <ayodejiadesegun20@gmail.com>
2024-05-09 13:06:12 +0100

    Initial commit

--- ayodejiAdesegun/shoppingSystem.ts
+++ ayodejiAdesegun/shoppingSystem.ts
@@ -1,20 +1,40 @@
 class shoppingSystem {
-    gala: Gala;
-    vodka: Vodka;
-    indomie: Indomie;
-    constructor(gala: Gala, vodka: Vodka, indomie: Indomie) {
-        this.gala = gala;
-        this.vodka = Vodka;
-        this.indomie = Indomie;
+    shoppingCart: shoppingCart;
+    constructor(shoppingCart: shoppingCart) {
+        this.shoppingCart = shoppingCart
     }
 };
+class shoppingCart {
+    sausage: Sausage;
+    vodka: Vodka;
+    noodles: Noodles;
+    constructor(sausage: Sausage, vodka: Vodka, noodles: Noodles) {
+        this.sausage = Sausage;
+        this.vodka = Vodka;
+        this.noodles = Noodles;
+    }
+};
+type sausageKind = "Gala" | "Rite" | "Bigi" | "SuperBite";
+class Sausage {
+    kind: sausageKind
+    constructor(kind: sausageKind) {
+        this.kind = kind;
+    };
+    add() {
 
-class Gala {
-
+    }
 };
 class Vodka {
 
 };
-class Indomie{
+type noodlesKind = "Indomie" | "Golden Penny" | "Chiki Chiki";
+class Noodles {
+    noodles: noodlesKind
+    constructor(noodles: noodlesKind) {
+        this.noodles = noodles;
+    }
+
+};
+class Always {
 
 };
