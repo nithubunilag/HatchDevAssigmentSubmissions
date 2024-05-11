2054992f1768109a4e5673737f515ac972095ed9
Farida Momoh <95109808+KimFarida@users.noreply.github.com>
2024-05-11 12:23:48 +0100

    Create selectionSort.ts

---
+++ Godsfavour Farida Momoh/week4/sortingAlgorithm/selectionSort.ts
@@ -0,0 +1,13 @@
+function selectionSort<T>(arr: T[]): T[] {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+  }
+  return arr;
+}
+
