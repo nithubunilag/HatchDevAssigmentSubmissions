4bd3f2a36c9840e489c02552ed822efdfd8d79c1
e-jessie <140353139+e-jessie@users.noreply.github.com>
2024-05-19 01:10:45 -0800

    Create flipAnImage.ts
    FlippingImage Assignment

---
+++ Ezike Jessica/Week5_assignment/flipAnImage.ts
@@ -0,0 +1,23 @@
+function flipAndInvertImage(image: number[][]): number[][] {
+    let flippedImage: number[][] = []
+    
+
+    for(let i=0; i<image.length; i++) {
+        let container: number[] = []
+
+        for(let j=0; j<image[i].length; j++) {
+            if(image[i][j] === 0) {
+                container.unshift(1)
+            }
+            else {
+                container.unshift(0)
+            }
+        }
+        
+        flippedImage.push(container)
+    }
+
+    return flippedImage;
+};
+
+console.log(flipAndInvertImage([[0,0,1], [0,1,0], [0,1,1]]))
