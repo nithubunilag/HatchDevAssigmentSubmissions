ec6815b500907e0392ca34c1694af81375b063fa
habullhigh <oyehabdullhigh@gmail.com>
2024-05-22 19:14:15 +0100

    Added minor changes to the linked list and made the whole doubly linked list

--- AbdullahOyetoro/linkedLists/index.ts
+++ AbdullahOyetoro/linkedLists/index.ts
@@ -6,146 +6,168 @@ class LNode<T>{
   }
 }
 
-class CircularLinkedList<T> {
-  head: LNode<T> | null = null;
-  private getcurrentNode(position?: number): LNode<T> | null {
-    if (!this.head) return null;
-    let currentNode = this.head;
-    if (position !== undefined) {
+class CircularLinkedList<T>{
+  head: LNode<T>
+  private getcurrentNode(position?:number){
+    let currentNode = this.head 
+    if(!this.head){
+      return currentNode
+    }
+    if(position){
       let currentPosition = 1;
-      while (currentPosition !== position) {
-        if (currentNode.next === this.head) {
-          console.log(`Reached the tail; the list has ${currentPosition} node(s).`);
-          return currentNode;
+      while(currentPosition !== position){
+        if(currentNode.next == this.head){
+          console.log(`the loop has reached the tail and the linked list has ${currentPosition} node(s)`)
+          return currentNode
         }
-        if (position === -1 && currentNode.next && currentNode.next.next === this.head) {
-          console.log(`Reached the penultimate node at position ${currentPosition}`);
-          return currentNode;
+        if(position ==-1 && currentNode.next.next ==this.head){
+          console.log(`reached the penultimate node at position ${currentPosition}`)
+          return currentNode
         }
-        if (currentPosition === (position - 1)) {
-          return currentNode;
+        
+        else if (currentPosition == (position-1)){
+          // console.log(`its reached the position before the desired node`)
+          return currentNode
         }
-        if (currentNode.next) {
-          currentNode = currentNode.next;
-          currentPosition++;
-        } else {
-          break;
-        }
-      }
-    } else {
-      while (currentNode.next && currentNode.next !== this.head) {
         currentNode = currentNode.next;
+        currentPosition++
+      }  
+    }
+    else{
+      while(currentNode.next&& currentNode.next !==this.head){
+        currentNode = currentNode.next
       }
-      return currentNode;
+      //if the while loop runs we are at the last node whose next node is the head
+      return currentNode
     }
-    return currentNode;
+    return currentNode
   }
-
-  getlistlength(): number {
-    if (!this.head) return 0;
-    if (this.head.next === this.head) return 1;
-    let currentLength = 1;
-    let currentNode = this.head;
-    while (currentNode.next && currentNode.next !== this.head) {
-      currentNode = currentNode.next;
-      currentLength++;
+  getlistlength(){
+    if(!this.head){
+      return 0
     }
-    return currentLength;
+    if(this.head.next == this.head){
+      return 1
+    }
+    let currentLength = 1
+    let currentNode =this.head
+    while(currentNode.next&& currentNode.next !== this.head){
+      currentNode = currentNode.next
+      currentLength ++
+    }
+    return currentLength
   }
-
-  append(val: T): void {
-    let newNode = new LNode(val);
-    if (!this.head) {
-      this.head = newNode;
-      this.head.next = this.head; // Make it circular
-      return;
+  append(val:T){
+    //if no head
+    let newNode = new LNode(val)
+    if(!(this.head)){
+      this.head = newNode
+      this.head.next = this.head;//adding the to the next to make it circular
+      return
     }
+    //if theres a head
     let currentNode = this.getcurrentNode();
-    if (currentNode) {
-      currentNode.next = newNode;
-      newNode.next = this.head;
-    }
+    //this calls the getcurrenet node function which gets the last node
+    //the node whose next node points to the head
+    currentNode.next = newNode
+    currentNode = currentNode.next
+    currentNode.next = this.head
+    return
   }
-
-  prepend(val: T): void {
+  prepend(val:T){
     let newNode = new LNode(val);
-    if (!this.head) {
-      this.head = newNode;
-      this.head.next = this.head;
-      return;
+    let oldHead = this.head;
+    if(!this.head){
+      this.head = newNode
+      this.head.next = newNode
+      return
     }
-    let currentNode = this.getcurrentNode();
-    if (currentNode) {
-      newNode.next = this.head;
-      this.head = newNode;
-      currentNode.next = this.head;
+    if(this.head.next ==this.head){
+      this.head.next = newNode;
+      this.head = newNode
+      newNode.next = oldHead
+      return
     }
+    this.head = newNode
+    newNode.next = oldHead
   }
-
-  add(val: T, position?: number): void {
-    if (!this.head) {
-      this.head = new LNode(val);
-      this.head.next = this.head;
-      return;
+  add(val:T, position?:number){
+    if(!this.head){
+      let newNode = new LNode(val)
+      this.head =newNode
     }
-    if (position === undefined || position <= 1) {
-      this.prepend(val);
-      return;
+    if(!position){
+      this.append(val)
+      return
     }
-    let newNode = new LNode(val);
-    let length = this.getlistlength();
-    if (position > length) {
-      console.log(`Invalid position`);
-      return;
+    if (position&& position <=1&&position >=0){
+      this.prepend(val)
+      return
     }
-    let currentNode = this.getcurrentNode(position);
-    if (currentNode) {
-      newNode.next = currentNode.next;
-      currentNode.next = newNode;
-    }
-  }
-
-  deletebyPosition(position?: number): void {
-    if (!this.head || this.head.next === this.head) {
-      console.log(`Can't delete from a list with one or no nodes.`);
-      this.head = null;
-      return;
-    }
-    if (!position || position <= 1) {
-      let currentNode = this.getcurrentNode();
-      if (currentNode && this.head) {
-        currentNode.next = this.head.next;
-        this.head = this.head.next;
+    let newNode = new LNode(val)
+    if(position && position>1){
+      let length = this.getlistlength()
+      if(position <= length){
+        let currentNode = this.getcurrentNode(position);
+        newNode.next = currentNode.next
+        currentNode.next = newNode
+        return 
       }
-      return;
-    }
-    let currentNode = this.getcurrentNode(position);
-    if (currentNode && currentNode.next) {
-      if (currentNode.next === this.head) {
-        console.log('This is the last node in the list');
-        let penultimateNode = this.getcurrentNode(-1);
-        if (penultimateNode) {
-          penultimateNode.next = this.head;
-        }
-      } else {
-        currentNode.next = currentNode.next.next;
+      else{
+        console.log(`Invalid position`)
+        return 
       }
     }
   }
-
-  deleteByVal(val: T): void {
-    if (!this.head) return;
-    let currentNode = this.head;
-    let currentPosition = 1;
-    do {
-      if (currentNode.data === val) {
-        this.deletebyPosition(currentPosition);
-        return;
+  deletebyPosition(position?:number,val?:T){
+    if(!this.head ||this.head.next ==this.head){
+      return `this cant delete from a list with one node`
+    }
+    if(!position ||this.head ==this.head.next){
+      //automatically deletes from the back
+      
+      let penultimateNode = this.getcurrentNode(-1)
+      penultimateNode.next = this.head
+    }
+    if(position){
+      // this gets the node before the position
+      let currentNode = this.getcurrentNode(position)
+      if(currentNode.next == this.head){
+        console.log('This is the last Node in the list')
+        let penultimateNode = this.getcurrentNode(-1)
+        penultimateNode.next = currentNode.next
+        return
       }
-      currentNode = currentNode.next;
-      currentPosition++;
-    } while (currentNode && currentNode !== this.head);
+      
+      currentNode.next = currentNode.next.next
+      
+    }
   }
+   private getcurrNodeByVal (val:T){
+    if(!this.head)return ;
+    let currentNode = this.head
+    let currentPosition = 1
+    while(currentNode.next){
+      if(currentNode.data == val)return currentPosition ;
+      if(currentNode.next == this.head){
+        console.log("reached the end of the linked list")
+        break
+      }
+      currentNode = currentNode.next
+      currentPosition++
+    }
+    if (currentNode == this.head){
+      return -2
+    } 
+  }
+  deleteByVal(val:T){
+    if(!this.head)return;
+    let position =this.getcurrNodeByVal(val)
+    if (position&& position !== -2){
+      this.deletebyPosition(position)
+      return
+    }
+ }
 }
 
 const clist2 = new CircularLinkedList<string>();
