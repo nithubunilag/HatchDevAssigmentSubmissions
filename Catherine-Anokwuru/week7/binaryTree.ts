24adecdf8dbaf6a66041bf8f33affd4c6be95698
catherine-anokwuru <catherineanokwuru@gmail.com>
2024-05-27 18:07:01 +0100

    binary tree

--- Catherine-Anokwuru/week7/binaryTree.ts
+++ Catherine-Anokwuru/week7/binaryTree.ts
@@ -1,3 +1,7 @@
+// name - Catherine Ihuoma Anokwuru
+// email - catherineanokwuru@gmail.com
+// github - Catherine-Anokwuru
+
 class TNode<T> {
   data: T;
   lp: TNode<T> | null;
@@ -12,40 +16,68 @@ class TNode<T> {
 
 class BT<T> {
   root: TNode<T> | null;
-  //  queue: []
-  // travArr: [];
 
   constructor() {
     this.root = null;
-    // this.queue = this.queue.push(this.root)
   }
-  
 
-  traverse() {
+  insert(val: T) {
+    const newNode = new TNode(val);
+
     if (!this.root) {
+      this.root = newNode;
       return;
+    }
+    let queue: TNode<T>[] = [];
+    queue.push(this.root);
+
+    while (queue.length > 0) {
+      let current = queue.shift();
+
+      if (current!.lp === null) {
+        current!.lp = newNode;
+        return;
+      } else if (current!.rp === null) {
+        current!.rp = newNode;
+        return;
+      } else {
+        queue.push(current!.lp);
+        queue.push(current!.rp);
+      }
+    }
+  }
+
+  traverse(): T[] {
+    if (!this.root) {
+      return[];
     }
 
     let queue: TNode<T>[] = [];
     queue.push(this.root);
 
-    let travArr: TNode<T>[] = [];
+    let travArr: T[] = [];
 
     while (queue.length > 0) {
-      // let removedNode = queue.unshift()
       let node = queue.shift();
 
-      if (node) {
-        travArr.push(node);
-        if (node.lp) {
-          queue.push(node.lp);
+        travArr.push(node!.data);
+        if (node!.lp) {
+          queue.push(node!.lp);
         }
-        if (node.rp) {
-          queue.push(node.rp);
+        if (node!.rp) {
+          queue.push(node!.rp);
         }
-      }
     }
-
     return travArr;
   }
+
+
 }
+
+const tree = new BT()
+tree.insert('a')
+tree.insert('b')
+tree.insert('c')
+tree.insert('d')
+console.log(tree.traverse());
+
