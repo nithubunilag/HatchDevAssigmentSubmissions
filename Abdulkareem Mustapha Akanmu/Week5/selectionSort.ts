a5bc9414a8615ee55b6a2b1c0c6cab1498dd2354
Aphatheology <aphatheology@gmail.com>
2024-05-30 06:13:43 +0100

    Week5 assignment

---
+++ Abdulkareem Mustapha Akanmu/Week5/selectionSort.ts
@@ -0,0 +1,25 @@
+function selectionSort<T>(array: T[]): T[] {
+  let n = array.length;
+
+  for (let i = 0; i < n - 1; i++) {
+      let minIndex = i;
+
+      for (let j = i + 1; j < n; j++) {
+          if (array[j] < array[minIndex]) {
+              minIndex = j;
+          }
+      }
+
+      if (minIndex !== i) {
+          [array[i], array[minIndex]] = [array[minIndex], array[i]];
+      }
+  }
+
+  return array;
+}
+
+const unsortedArray3 = [64, 34, 25, 12, 22, 11, 90];
+console.log("Unsorted array (Selection Sort):", unsortedArray3);
+
+const sortedArray3 = selectionSort(unsortedArray3);
+console.log("Sorted array (Selection Sort):", sortedArray3);
