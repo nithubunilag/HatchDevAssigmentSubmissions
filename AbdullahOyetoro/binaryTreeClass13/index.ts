936079832f300136f72d092dd204d40274e1327d
habullhigh <oyehabdullhigh@gmail.com>
2024-05-31 11:25:52 +0100

    added traversal method usind DFS inorder traversal

--- AbdullahOyetoro/binaryTreeClass13/index.ts
+++ AbdullahOyetoro/binaryTreeClass13/index.ts
@@ -17,46 +17,47 @@ class BinaryT<T>{
     const newNode = new tNode(val);
     if (!this.root){
       this.root = newNode;
-      console.log("Pushed root value to queue")
+      // console.log("Pushed root value to queue")
       return;
     }
     let queue:tNode<T>[]=[];
     // console.log(queue)
     queue.push(this.root);
    
-    console.log("start")
-    console.log(queue)
+    // console.log("start")
+    // console.log(queue)
     while (queue.length >0){
       let currentNode =queue.shift();
       if(currentNode!.left === null){
-        console.log("left")
+        // console.log("left")
         currentNode!.left = newNode
         return;
       }
       else if(currentNode!.right === null){
-        console.log("left filled, right")
+       // console.log("left filled, right")
         currentNode!.right = newNode
         return;
       }
       else{
-        console.log("both filled, new root",queue)
+        // console.log("both filled, new root",queue)
         queue.push(currentNode!.left);
         queue.push(currentNode!.right);
       }
-      console.log("Next iteration queue",queue)
+      // console.log("Next iteration queue",queue)
     }
   }
+ 
   traversal(){
     if(!this.root){
       return
     }
-    let travArray:tNode<T>[]= [];
+    let travArray:T[]= [];
 
     let queue:tNode<T>[]= [];
     queue.push(this.root)
     while(queue.length>0){
       let currentNode = queue.shift()
-      travArray.push(currentNode!)
+      travArray.push(currentNode!.data)
       if(currentNode!.left !== null){
         queue.push(currentNode!.left)
       }
@@ -66,27 +67,50 @@ class BinaryT<T>{
     }
     return travArray
   }
+  traversalDFS(){
+    if (!this.root){
+      return
+    }
+
+    let travArray :T[] = [];
+    let stack  :tNode<T>[] = [];
+    let currentNode:tNode<T>|null = this.root ;
+
+    while (stack.length > 0 || currentNode !== null) {
+      while (currentNode !== null) {
+        stack.push(currentNode);
+        currentNode = currentNode.left;
+      }
+     
+      currentNode = stack.pop()!;
+      travArray.push(currentNode.data);
+      currentNode = currentNode.right;
+    }
+    return travArray
+
+  }
 }
 const bt5 = new BinaryT()
 bt5.insert(1)
-bt5.insert(2)
+bt5.insert(2)  
 bt5.insert(3)
 
-// bt5.insert(4)
-console.log(bt5.traversal())
-// bt5.insert(5)
-// bt5.insert(6)
-// bt5.insert(7)
-// bt5.insert(8)
-// bt5.insert(9)
-// bt5.insert(10)
-// bt5.insert(11)
-// bt5.insert(12)
-// bt5.insert(13)
-// bt5.insert(14)
-// bt5.insert(15)
-// bt5.insert(16)
+bt5.insert(4)
+bt5.insert(5)
+bt5.insert(6)
+bt5.insert(7)
+bt5.insert(8)
+bt5.insert(9)
+bt5.insert(10)
+bt5.insert(11)
+bt5.insert(12)
+bt5.insert(13)
+bt5.insert(14)
+bt5.insert(15)
+bt5.insert(16)
 // bt5.insert(17)
+console.log(bt5.traversal())
+console.log(bt5.traversalDFS())
 
 
 /**BINARY TREE
