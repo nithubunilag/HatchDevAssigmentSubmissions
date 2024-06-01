b1b18a90e2de3c17eb22f8237e144cfb942ac7e8
Quadri Naheem <naheemquadri3410@gmail.com>
2024-06-01 11:12:11 +0100

    feat:quick sort algorithm added

--- Quadri Naheem/sorting_algorithms/sorting_algorithms.ts
+++ Quadri Naheem/sorting_algorithms/sorting_algorithms.ts
@@ -111,6 +111,45 @@ function insertionSort(arr: number[]): number[] {
     return arr;
 }
 
+
+function quicksort(arr: number[]): void {
+    const stack: [number, number][] = [[0, arr.length - 1]];
+
+    while (stack.length) {
+        const [low, high] = stack.pop()!;
+        if (low < high) {
+            const pivotIndex = partition(arr, low, high);
+            stack.push([low, pivotIndex - 1]);
+            stack.push([pivotIndex + 1, high]);
+        }
+    }
+}
+
+//used in quick sort
+function partition(arr: number[], low: number, high: number): number {
+    const pivot = arr[low];
+    let left = low + 1;
+    let right = high;
+
+    while (true) {
+        while (left <= right && arr[left] <= pivot) {
+            left++;
+        }
+        while (left <= right && arr[right] >= pivot) {
+            right--;
+        }
+        if (left <= right) {
+            [arr[left], arr[right]] = [arr[right], arr[left]];
+        } else {
+            break;
+        }
+    }
+
+    [arr[low], arr[right]] = [arr[right], arr[low]];
+    return right;
+}
+
+
 const decimalNumbers = [3.2, 5.8, 3.99, 7.8, 4, 0.7, 6.5, 5.55, 8.8];
 const numbers = [4,90,6,12,87,4,6,3,0,9,6,2,89,543];
 
@@ -118,4 +157,5 @@ const sortedUsingBubblesort = bubbleSort(numbers);
 const sortedUsingMergesort = mergeSort(decimalNumbers);
 const sortedUsingSelectionsort = selectionSort(numbers);
 const sortedUsingInsertionsort = insertionSort(decimalNumbers);
-console.log(sortedUsingInsertionsort);
+const sortedUsingQuicksort = quicksort(numbers);
+console.log(numbers)//for quicksort
