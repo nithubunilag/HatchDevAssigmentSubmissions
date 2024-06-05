1ae3f627a34987e0c28b5fac93bb18d0a29c4e98
dev-xero <elo48858@gmail.com>
2024-06-05 22:14:00 +0100

    algo: Solve in order traversal problem

---
+++ Elochukwu Gerald Okafor/week-8/leetcode/leetcode-inorder.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+// IN ORDER: LEFT -> PARENT -> RIGHT
+function inorderTraversal(root: TreeNode | null): number[] {
+    // base case: if root is null, return
+   if (root == null) return [];
+   
+   // results
+   const nodes = [];
+   
+   // append the left children if present
+   if (root.left) nodes.push(...inorderTraversal(root.left));
+   
+   // append the parent in-between
+   nodes.push(root.val)
+   
+   // append the right children if present
+   if (root.right) nodes.push(...inorderTraversal(root.right));
+
+   return nodes;
+};
