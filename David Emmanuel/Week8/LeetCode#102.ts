d8396019af486c080331c4f52b501e9bb799558b
David Emmanuel <david3emmanuel@gmail.com>
2024-06-05 20:53:13 +0100

    LeetCode Problems
    Added screenshots

--- David Emmanuel/Week8/LeetCode#102.ts
+++ David Emmanuel/Week8/LeetCode#102.ts
@@ -1,13 +1,16 @@
-class TreeNode {
-    val: number
-    left: TreeNode | null
-    right: TreeNode | null
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.left = (left===undefined ? null : left)
-        this.right = (right===undefined ? null : right)
-    }
-}
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
 
 function levelOrder(root: TreeNode | null): number[][] {
     if (!root) return [];
@@ -17,7 +20,7 @@ function levelOrder(root: TreeNode | null): number[][] {
     let nextLevelRep: TreeNode | null = root;
     
     while (queue.length > 0) {
-        const current = queue.shift()!;
+        const current = queue.shift();
         if (current === nextLevelRep) {
             traversal.push([]);
             nextLevelRep = null;
