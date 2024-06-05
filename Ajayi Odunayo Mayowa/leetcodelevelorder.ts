dbbe4f19a6c8f5d258e7e0430bc9b2cd3b5b81c7
Odunayo <38246355+odunayo222@users.noreply.github.com>
2024-06-04 22:48:06 -0700

    Create leetcodelevelorder.ts

---
+++ Ajayi Odunayo Mayowa/leetcodelevelorder.ts
@@ -0,0 +1,52 @@
+class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val);
+        this.left = (left === undefined ? null : left);
+        this.right = (right === undefined ? null : right);
+    }
+}
+
+function levelOrder(root: TreeNode | null): number[][] {
+    if (!root) {
+        return [];
+    }
+
+    const ans: number[][] = [];
+    const q: TreeNode[] = [root];
+
+    while (q.length > 0) {
+        const currLevel: number[] = [];
+        const levelSize = q.length;
+
+        for (let i = 0; i < levelSize; i++) {
+            const node = q.shift()!;
+            currLevel.push(node.val);
+            if (node.left) {
+                q.push(node.left);
+            }
+            if (node.right) {
+                q.push(node.right);
+            }
+        }
+
+        ans.push(currLevel);
+    }
+
+    return ans;
+}
+
+
+const root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+root.right.left = new TreeNode(6);
+root.right.right = new TreeNode(7);
+
+const result = levelOrder(root);
+console.log(result); 
