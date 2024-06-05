71d15cf66ff8c19d4eaf93a7f62eb8255bc402e6
PetJs <peterfagoroye76@gmail.com>
2024-06-05 10:28:32 +0100

    Adding Leet Code Assignment

---
+++ Fagoroye Peter/LeetCode Assignments/leetCode144.ts
@@ -0,0 +1,24 @@
+// Binary Tree Preorder Traversal
+
+function preorderTraversal(root: TreeNode | null): number[] {
+    let result: number[] = [];
+
+    if (root === null) {
+        return result;
+    }
+
+    let stack: TreeNode[] = [root];
+
+    while(stack.length > 0){
+        let current = stack.pop()!;
+        result.push(current.val!);
+
+        if(current.right != null){
+            stack.push(current.right);
+        }
+        if(current.left != null){
+            stack.push(current.left)
+        }
+    }
+    return result;
+};
