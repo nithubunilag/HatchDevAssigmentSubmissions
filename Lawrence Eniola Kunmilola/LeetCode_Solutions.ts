53d048740ac32e759ee91e2173f00610dfcfa56c
Lawrence <lawrencekunmilola@gmail.com>
2024-06-06 11:28:45 +0100

    Update LeetCode_Solutions

--- Lawrence Eniola Kunmilola/LeetCode_Solutions.ts
+++ Lawrence Eniola Kunmilola/LeetCode_Solutions.ts
@@ -15,7 +15,7 @@
  * }
  */
 
-////
+////94. Binary Tree Inorder Traversal
 function inorderTraversal(root: TreeNode | null, value:Array<number>=[]): number[] {
     if(root) {
     inorderTraversal(root.left, value);
@@ -68,4 +68,30 @@ function levelOrder(root: TreeNode | null): number[][] {
     }
     
     return result;
+}
+
+/////450. Delete Node in a BST
+function deleteNode(root: TreeNode | null, key: number): TreeNode | null {
+    if (!root) return null;
+    
+    if (key < root.val) {
+        root.left = deleteNode(root.left, key);
+    } else if (key > root.val) {
+        root.right = deleteNode(root.right, key);
+    } else {
+        if (!root.left) return root.right;
+        if (!root.right) return root.left;
+        
+        let minLargerNode = getMin(root.right);
+        root.val = minLargerNode.val;
+        root.right = deleteNode(root.right, minLargerNode.val);
+    }
+    return root;
+}
+
+function getMin(node: TreeNode): TreeNode {
+    while (node.left !== null) {
+        node = node.left;
+    }
+    return node;
 }
