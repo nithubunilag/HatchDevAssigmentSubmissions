439a7b08496fbe8f08466bbfb5915a9275ebe7fb
Alexa <onabanjotomiwa@gmail.com>
2024-06-08 12:13:19 +0100

    Algorithms loading

---
+++ Onabanjo_Oluwatomiwa/Sorting Algorithm/selectionSort.ts
@@ -0,0 +1,18 @@
+function selectionsort(arr: number[]) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+  return arr;
+}
+
+console.log(selectionsort([5, 4, 3, 2, 1]));
