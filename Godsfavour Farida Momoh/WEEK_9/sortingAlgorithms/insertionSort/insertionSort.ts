4360a3e91f38104a350a60e9ca423adb66cf112b
Farida Momoh <95109808+KimFarida@users.noreply.github.com>
2024-06-08 01:44:31 +0100

    Create insertionSort.ts

---
+++ Godsfavour Farida Momoh/WEEK_9/sortingAlgorithms/insertionSort/insertionSort.ts
@@ -0,0 +1,14 @@
+function insertionSort(arr:number[]){
+    for (let i = 1; i < arr.length; i++){
+        let key = arr[i];
+        let j = i - 1
+
+        while(j >= 0 && arr[j] > key){
+            arr[j + 1] = arr[j]
+            j = j - 1
+        }
+
+        arr[j + 1] = key
+
+    }
+}
